0d6781649e8be6a503be392561e1650d3bbb1500
nagolonus <102842390+nagolonus@users.noreply.github.com>
2022-04-01 23:13:39 -0400

    Notes

--- Sierpinski.py
+++ Sierpinski.py
@@ -3,20 +3,24 @@ import random
 pen = turtle.Turtle()
 pen.speed(0)
 
-
+#draws a single point at the given coordinates
 def draw(x,y):
     pen.penup()
     pen.goto(x,y)
     pen.dot()
 
+#makes the Sierpinski fractal
 def Sierpinski():
+    #the initial triangle and subsequent points
     tri = [(0,250),(-250,-125),(250,-125)]
     others = []
-
+    
+    #Draws the initial triangle
     for point in tri:
         draw(point[0],point[1])
-
+    #Main loop that draws the sierpinski triangle with a given number of dots
     for i in range(0,10000):
+        #If there are no other points, grab two points from the initial triangle. Got some W.E.T. code here
         if len(others)==0:
             firstPoint=random.randrange(0,3)
             secPoint=random.randrange(0,3)
